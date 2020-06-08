df08b0cef93ec3112dbd4942dd4ee9f9e5d1e3c4
Jaime Gensler <jaimegensler0@gmail.com>
2020-06-07 17:21:10 -0700

    idk anymore

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,8 +6,8 @@ import MenuButton from './MenuButton';
 export default function Header() {
     const [menuExtended, setMenuExtended] = useState(false);
     return (
-        <header className="h-16 shadow-md">
-            <div className="flex h-12 px-8 space-x-8 bg-white text-blue-800">
+        <header className="h-16 shadow-md text-blue-800">
+            <div className="flex h-12 px-8 space-x-8 bg-white">
                 <h1 className="flex flex-grow sm:flex-grow-0 items-center font-bold text-2xl">
                     J Gensler
                 </h1>
@@ -19,7 +19,7 @@ export default function Header() {
                 <Media />
             </div>
 
-            <div className="h-4 text-blue-800 bg-green-500 bg-split" />
+            <div className="h-4 bg-green-500 bg-split" />
         </header>
     );
 }
---
+++ pages/about.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+    return <div>Should probably contain info about me in the future</div>;
+}
